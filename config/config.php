15ba55980af4c5b99ee83b9aa3481ae8d47293cb
amir.salehi <iamamirsalehi@gmail.com>
2021-04-11 10:52:05 +0430

    config file updated

--- config/config.php
+++ config/config.php
@@ -3,14 +3,14 @@
 return [
     'maximum_withdraw' => 20000000,
 
-    'maximum_deposit'  => 50000000,
+    'maximum_deposit' => 50000000,
 
     'minimum_withdraw' => 10000,
 
-    'minimum_deposit'  => 100000,
+    'minimum_deposit' => 100000,
 
-    'repositories'     => [
-        'coin'    => \Iamamirsalehi\LaravelBalance\Models\Coin::class,
+    'repositories' => [
+        'coin' => \Iamamirsalehi\LaravelBalance\Models\Coin::class,
         'balance' => \Iamamirsalehi\LaravelBalance\Models\Balance::class,
         'withdraw' => \Iamamirsalehi\LaravelBalance\Models\Withdraw::class,
         'deposit' => \Iamamirsalehi\LaravelBalance\Models\Deposit::class,
