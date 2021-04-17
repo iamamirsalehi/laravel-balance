6b7346ebe21d089a4c4e1ffe779b80e3be673c15
amir.salehi <iamamirsalehi@gmail.com>
2021-04-17 14:38:21 +0430

    Balance issue fixed

--- src/LaravelBalanceServiceProvider.php
+++ src/LaravelBalanceServiceProvider.php
@@ -29,10 +29,10 @@ class LaravelBalanceServiceProvider extends ServiceProvider
             ], 'seeders');
 
             $this->publishes([
-                __DIR__.'/Models/Balance.php/' => app_path('Models'),
-                __DIR__.'/Models/Coin.php/' => app_path('Models'),
-                __DIR__.'/Models/Deposit.php/' => app_path('Models'),
-                __DIR__.'/Models/Withdraw.php/' => app_path('Models'),
+                __DIR__. '\Models\Balance.php' => app_path('Models'),
+                __DIR__.'\Models\Coin.php' => app_path('Models'),
+                __DIR__.'\Models\Deposit.php' => app_path('Models'),
+                __DIR__.'\Models\Withdraw.php' => app_path('Models'),
             ], 'Models');
         }
     }
